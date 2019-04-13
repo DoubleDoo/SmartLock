49534b7b0ac0357f4ceb079a4c59371a8e54cc5a
Dmitry Dubina <dima89260755725@gmail.com>
2019-04-14 01:39:23 +0300

    Add proteus file and created avr code

---
+++ Code/SmartLock/SmartLock/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SmartLock.cpp
+ *
+ * Created: 14.04.2019 1:38:07
+ * Author : dima8
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
