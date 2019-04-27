7f5192ea38d870b31dfb6332621da048ad47cfa7
Dmitry Dubina <dima89260755725@gmail.com>
2019-04-28 02:08:38 +0300

    new proteus scheme developed

--- Android_code/app/src/main/java/com/example/smartlock/MainActivity.java
+++ Android_code/app/src/main/java/com/example/smartlock/MainActivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       // requestWindowFeature(Window.FEATURE_NO_TITLE);
+      //  getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+       //         WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 
@@ -17,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
 public void register(android.view.View view)
 {
-    ((TextView)findViewById(R.id.str)).setX(1);
     Intent intent = new Intent(MainActivity.this, SecondActivity.class);
     startActivity(intent);
 }
